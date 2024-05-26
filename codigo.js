941071490681dd54b63a418b5070f78737fe56d8
Jorge Asdrubal <jaog.11.2003@gmail.com>
2024-05-26 12:28:03 -0500

    Logica JS con funcion de sumar.

--- codigo.js
+++ codigo.js
@@ -1,9 +1,20 @@
 let cont = 0;
 
 const $h1 = document.querySelector("h1");
+const $button = document.getElementById("restar");
 $h1.textContent = cont;
 
 function sumar(valor) {
 
+    if (valor === 0) {
+        cont = 0;
+        $h1.textContent = cont;
+        return;
+    }
 
+    cont += valor;
+    
+    if (cont < 0) cont = 0
+
+    $h1.textContent = cont;
 }
